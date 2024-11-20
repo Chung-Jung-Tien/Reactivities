ab4778451585794882ea3fbc73607e1035fa6e51
AndrewChung <enrichme@hotmail.com>
2024-11-20 17:26:00 +0800

    End of section 18

---
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { Button, ButtonContent, Grid, Header } from "semantic-ui-react";
+import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
+import 'cropperjs/dist/cropper.css';
+
+interface Props {
+    loading: boolean;
+    uploadPhoto: (file: Blob) => void;
+}
+
+export default function PhotoUploadWidget({loading, uploadPhoto}: Props) {
+    const [files, setFiles] = useState<any>([]);
+    const [cropper, setCropper] = useState<Cropper>();
+
+    function onCrop() {
+        if (cropper) {
+           cropper.getCroppedCanvas().toBlob(blob => uploadPhoto(blob!));
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            //if there is an type error, use this
+            // files.forEach((file: object & {preview?: string}) => URL.revokeObjectURL(file.preview!)); 
+            files.forEach((file: any) => URL.revokeObjectURL(file.preview));
+        }
+    }, [files])
+
+    return (
+        <Grid>
+           <Grid.Column width={4}>
+                <Header color="teal" content='Step 1 - Add Photo'></Header>
+                <PhotoWidgetDropzone setFiles = {setFiles}/>
+            </Grid.Column> 
+            <Grid.Column width={1} />
+           <Grid.Column width={4}>
+                <Header color="teal" content='Step 2 - ReSize Image'></Header>
+                {files && files.length > 0 && (
+                    <PhotoWidgetCropper setCropper={setCropper} imagePreview={files[0].preview}/>
+                )}
+            </Grid.Column> 
+            <Grid.Column width={1} />
+           <Grid.Column width={4}>
+                <Header color="teal" content='Step 3 - Preview and Upload'></Header>
+                {files && files.length > 0 && 
+                    <>
+                        <div className="image-preview" style={{minHeight: 200, overflow: 'hidden'}}></div>
+                        <Button.Group widths={2}>
+                            <Button loading={loading} onClick={onCrop} positive icon='check'/>
+                            <Button disabled={loading} onClick={() => setFiles([])} positive icon='close'/>
+                        </Button.Group>
+                    </>
+                }
+            </Grid.Column> 
+        </Grid>
+    )
+}
--- client-app/src/router/Routes.tsx
+++ client-app/src/router/Routes.tsx
@@ -7,6 +7,7 @@ import TestErrors from "../features/errors/TestError";
 import NotFound from "../features/errors/NotFound";
 import ServerError from "../features/errors/ServerError";
 import LoginForm from "../Users/LoginForm";
+import ProfilePage from "../features/profiles/ProfilePage";
 
 export const routs: RouteObject[] = [
     {
@@ -17,6 +18,7 @@ export const routs: RouteObject[] = [
             {path: 'activities/:id', element: <ActivityDetails/>},
             {path: 'createActivity', element: <ActivityForm key='create'/>},
             {path: 'manage/:id', element: <ActivityForm key='manage'/>},
+            {path: 'profiles/:username', element: <ProfilePage/>},
             {path: 'login', element: <LoginForm/>},
             {path: 'errors', element: <TestErrors/>},
             {path: 'not-found', element: <NotFound/>},
